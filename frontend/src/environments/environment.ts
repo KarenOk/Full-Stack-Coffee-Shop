8cf2952510fc1d28bbfe146709e8bfb2668211e9
karenok <karenokonkwo29@gmail.com>
2020-08-25 00:45:35 +0000

    update environment variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,12 +3,12 @@
  */
 
 export const environment = {
-  production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
-  auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
-  }
+	production: false,
+	apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
+	auth0: {
+		url: "fullstack-coffee-shop-udacity.us", // the auth0 domain prefix
+		audience: "CoffeeShop", // the audience set for the auth0 app
+		clientId: "SJ7yVWIfjGLGb6lqmHSM8Qm3L4eoS7oW", // the client id generated for the auth0 app
+		callbackURL: "http://localhost:8100", // the base url of the running ionic application.
+	},
 };
